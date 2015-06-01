5622769aaf3f3b9ad66846f7eb221599be21a4ec
Matthew Craig <mattwcraig@gmail.com>
2015-05-31 19:26:12 -0500

    Draft of bash build script

---
+++ build_bdist.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+base=$PWD
+
+
+to_build_recipe=recipes/[a-z]*
+for recipe in $to_build_recipe
+do
+    CONDA_PY=2.7 conda build $recipe || echo "Failed on $recipe"
+done
+
+cd bdist_conda
+to_build=[a-zA-Z]*
+for d in $to_build
+    do
+        cd $d
+        python setup.py bdist_conda || echo "Failed on $PWD"
+        cd ..
+    done
+
+cd $base
+
