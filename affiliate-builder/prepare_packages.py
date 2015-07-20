ec43c3531ca9289a5975ea277956f119832079a4
Matthew Craig <mattwcraig@gmail.com>
2015-07-20 11:28:10 -0500

    Fix platform names in prepare_packages

--- affiliate-builder/prepare_packages.py
+++ affiliate-builder/prepare_packages.py
@@ -27,7 +27,7 @@ BDIST_CONDA_FOLDER = 'bdist_conda'
 TEMPLATE_FOLDER = 'recipe_templates'
 RECIPE_FOLDER = 'recipes'
 BUILD_ORDER = 'build_order.txt'
-ALL_PLATFORMS = ['osx-64', 'linux', 'win32', 'win64']
+ALL_PLATFORMS = ['osx-64', 'linux-64', 'linux-32', 'win-32', 'win-64']
 
 class Package(object):
     """
