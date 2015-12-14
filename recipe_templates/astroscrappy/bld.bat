46b1a119879b14ce9fdaa0b275d0ed60dc82abcf
Matthew Craig <mattwcraig@gmail.com>
2015-12-13 21:13:27 -0600

    Typo fix in header file name

--- recipe_templates/astroscrappy/bld.bat
+++ recipe_templates/astroscrappy/bld.bat
@@ -1,6 +1,6 @@
 :: This is painfully ugly, but might make the build succeed on python 2.7
 
-copy "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include\stdinc.h" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\include"
+copy "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include\stdint.h" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\include"
 
 "%PYTHON%" setup.py install --offline
 if errorlevel 1 exit 1
