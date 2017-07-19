7245bd7d70163adcb55f6630fa75ce989ca507ed
Matthew Craig <mattwcraig@gmail.com>
2017-07-19 15:32:44 -0500

    Check identical versions for missing builds

--- extruder/copy_packages.py
+++ extruder/copy_packages.py
@@ -23,7 +23,7 @@ class PackageCopier(object):
             Name of source conda channel.
         destination : ``str``
             Name of destination conda channel.
-        package : ``dict``
+        input_package : ``dict``
             Dictionary in which keys are package names and values are either
             a string version number (e.g. ``'1.0.1'``) or ``None``, which
             indicates the latest version on the source channel should be
@@ -54,8 +54,12 @@ class PackageCopier(object):
 
         copy_versions = {}
         for p, version in packages.items():
+            copy_builds = []
             need_to_copy = False
-
+            # This will end up True if  the version exists on both src and dest
+            # and triggers a comparison of file names. Technically, it could
+            # be omitted, but seems more likely to be clear to future me.
+            check_builds = False
             cf = self.api.package(self.source, p)
             cf_version = VersionOrder(cf['latest_version'])
 
@@ -82,20 +86,63 @@ class PackageCopier(object):
                 if pinned_version is None:
                     if cf_version > ap_version:
                         need_to_copy = True
+                    elif cf_version == ap_version:
+                        check_builds = True
+
                 else:
                     if str(pinned_version) not in ap['versions']:
                         need_to_copy = True
+                    else:
+                        check_builds = True
+                if check_builds:
+                    # If we get here it means that the same version is on both
+                    # source and destination so we need to check the individual
+                    # builds.
+                    copy_builds = \
+                        self._check_for_missing_builds(cf,
+                                                       ap,
+                                                       cf_version)
+                    need_to_copy = len(copy_builds) > 0
             if need_to_copy:
-                copy_versions[p] = str(cf_version)
+                copy_versions[p] = (str(cf_version), copy_builds)
 
         return copy_versions
+
+    def _check_for_missing_builds(self, source, dest, version):
+        """
+        For two packages that have the same version, see if there are any
+        files on the source that are not on the destination.
+
+        source and dest are both conda channels, and version
+        should be a string.
+        """
+        def files_for_version(channel, version):
+            files = [f['basename'] for f in channel['files']
+                     if VersionOrder(version) == VersionOrder(f['version'])]
+            return files
+
+        source_files = files_for_version(source, version)
+        destination_files = files_for_version(dest, version)
+
+        need_to_copy = [src for src in source_files
+                        if src not in destination_files]
+
+        return need_to_copy
 
     def copy_packages(self):
         """
         Actually do the copying of the packages.
         """
         for p, v in self.to_copy.items():
-            self.api.copy(self.source, p, v, to_owner=self.destination)
+            version, buildnames = v
+            if not buildnames:
+                # Copy all of the builds for this version
+                self.api.copy(self.source, p, v, to_owner=self.destination)
+            else:
+                for build in buildnames:
+                    self.api.copy(self.source, p, version,
+                                  basename=build,
+                                  to_owner=self.destination)
 
 
 def main(arguments=None):
